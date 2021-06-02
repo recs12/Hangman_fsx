267077976b15d0dca7862d97d648d30b0b2c049e
Slimane RECHDI <47180175+recs12@users.noreply.github.com>
2021-06-02 13:13:15 -0400

    add some logic

--- pendu.fsx
+++ pendu.fsx
@@ -1,82 +1,93 @@
-
 open System
 
-module Counter =
+module Hangman =
 
-    type State = {
-        hidden: string;
-        secret: string;
-        picked': List<char>;
-    }
+    type State =
+        { mutable hidden: string
+          mutable secret: string
+          mutable picked': List<char> }
     //add tries = 12 later.
 
-    let init = {
-        hidden = "";
-        secret = "coconut";
-        picked' = [];
-        }
 
-
-    type Msg = Try
-
-
-    let update (msg: Msg) (state: State) : State =
-        match msg with
-        | Try -> { state with hidden = "_ _ _"; secret = "coco"; picked' = [] }
+    let init =
+        { hidden = ""
+          secret = "coconut"
+          picked' = [] }
 
 
     // Split word into list of char.
-    let splitIntoList text' : List<char> =
-        Seq.toList text'
+    let splitIntoList text' : List<char> = Seq.toList text'
 
 
     // Generate list from a word
     let createWordFromList lt' =
         //la = System.String.Concat()
         let lt = List.map string lt'
-        String.concat " " (Array.ofList(lt))
+        String.concat " " (Array.ofList (lt))
 
 
     // Gives the length of a given list.
-    let getLengthFromList lt' : int =
-        List.length lt'
+    let getLengthFromList lt' : int = List.length lt'
 
 
     let rec wordListWithSpaces word' =
         //["a"; "b" ; "c"] -> ["a "; "b "; "c"]
         match word' with
         | [] -> []
-        | [head] -> [head]
-        | x :: xs -> (x + " ")  :: wordListWithSpaces xs
+        | [ head ] -> [ head ]
+        | x :: xs -> (x + " ") :: wordListWithSpaces xs
+
 
     let inline replace list a b =
         list |> Seq.map (fun x -> if x = a then b else x)
 
-    let game (letter:string) (secretWord:string) (lettersYouCanPick:List<char>) (lettersAlreadyPicked:List<char>) =
-        //do something.
-        0
 
-    let rec foo secret' picked' =
+    let rec addUnderscores secret' picked' =
         match secret' with
         | [] -> []
-        | x :: xs -> (if not (List.contains x picked') then x else "_") :: foo xs picked'
+        | x :: xs ->
+            (if not (List.contains x picked') then
+                 x
+             else
+                 "_")
+            :: addUnderscores xs picked'
 
+    let transformationType lChars = 
+        lChars |> List.map (fun x -> string(x))
 
-    // List.contains 5 [2..2..10]
     let HideLetters secret picked' =
         let secret' = splitIntoList secret
-        0
+        // change a list of char to a list of string.
+        let secret'' = transformationType secret'
+        let display = addUnderscores secret'' picked'
+        // change the list to a word
+        // add space to the word
+        let word = createWordFromList display
+        word
 
+    let mutable CurrentState : State =
+        { init with
+              secret = init.secret
+              hidden = init.hidden
+              picked' = init.picked' }
 
-    let mutable CurrentState:State = {init with secret = init.secret; hidden = init.hidden; picked' = init.picked'} 
     let hiddenCharList = CurrentState.hidden
-    // isHidden = 
     let mutable playerTries = 12
-    while (playerTries > 0)  do
-        printfn "-Hidden: %A\n-Secret %A\n-Picked %A" CurrentState.hidden CurrentState.secret CurrentState.picked'
-        printfn "\nEnter a letter ?: "
-        printfn "to exit press (ctrl + C)"
-        let mutable x = System.Console.ReadLine()
+
+    while (playerTries > 0) do
+        printfn "\n(exit: ctrl + C) Enter a letter ?: "
+        printfn ""
+
+        let mutable x = char (System.Console.ReadLine())
+
+        if not (List.contains x CurrentState.picked') then
+            CurrentState.picked' <- x :: CurrentState.picked'
+
+        CurrentState.hidden <-  HideLetters CurrentState.secret (transformationType CurrentState.picked')
+
         playerTries <- playerTries - 1
+        printfn "-Hidden: %A\n-Secret %A\n-Picked %A" CurrentState.hidden CurrentState.secret CurrentState.picked'
+        printfn "Chances left: %A" playerTries
         0 |> ignore
-    printfn "You have exeeded your number of tries!"
+
+    printfn "You have exeeded 12 tries!"
